cdd7aad15c733c8622219b90d94cffd44c157ab6
alexa <alexa@DESKTOP-33ED89E>
2026-02-26 21:09:44 +0200

    Main completed

--- Habits Tracker/main.cpp
+++ Habits Tracker/main.cpp
@@ -1,20 +1,74 @@
-// Habits Tracker.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "HabitManager.h"
 #include <iostream>
+
+using namespace std;
+void showMenu()
+{
+    cout << "\n=== Habit Tracker ===\n";
+    cout << "1. Add Habit\n";
+    cout << "2. List Habits\n";
+    cout << "3. Mark Habit Done Today\n";
+    cout << "4. Show Stats\n";
+    cout << "5. Exit\n";
+    cout << "Choose an option: ";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HabitManager manager;
+    manager.loadFile("data / habits.txt");
+    int choice;
+    do
+    {
+        showMenu();
+        cin >> choice;
+        cin.ignore();
+        switch (choice) {
+        case 1: {
+            string name;
+            cout << "Enter habit name: ";
+            getline(std::cin, name);
+
+            if (name.empty()) {
+                cout << "Habit name cannot be empty.\n";
+                break;
+            }
+
+            manager.addHabit(Habit(name));
+            cout << "Habit added successfully!\n";
+            break;
+        }
+
+        case 2:
+            manager.listHabits();
+            break;
+
+        case 3: {
+            manager.listHabits();
+            cout << "Enter habit number to as mark done: ";
+
+            int index;
+            cin >> index;
+            cin.ignore();
+
+            manager.markHabitDone(index);
+            break;
+        }
+
+        case 4:
+            manager.showStatus();
+            break;
+
+        case 5:
+            manager.saveFile("data/habits.txt");
+            cout << "Habits saved. Goodbye!\n";
+            break;
+
+        default:
+            cout << "Invalid option. Try again.\n";
+        }
+
+    } while (choice != 5);
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
