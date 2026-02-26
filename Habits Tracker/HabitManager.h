f96a4d4fd3e79e5932477ac556e697a132f77f44
alexa <alexa@DESKTOP-33ED89E>
2026-02-26 20:01:54 +0200

    HabitManager class has been defined with all the needed members

--- Habits Tracker/HabitManager.h
+++ Habits Tracker/HabitManager.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef HABITMANAGER_H
+#define HABITMANAGER_H
+
+#include "Habit.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class HabitManager
+{
+private:
+	vector<Habit> habits;
+public:
+	void addHabit(const Habit& habit);
+	void listHabits()const;
+	void markHabitDone(int index);
+	void showStatus()const;
+	void saveFile(const string& filename)const;
+	void loadFile(const string& filename);
+};
+#endif
