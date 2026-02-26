7f3746c8867ab492b8260c85dcab86bc8da970c7
alexa <alexa@DESKTOP-33ED89E>
2026-02-26 19:55:55 +0200

    The Habit class is created with all the members needed

--- Habits Tracker/Habit.h
+++ Habits Tracker/Habit.h
@@ -1,4 +1,28 @@
 #ifndef HABIT_H
 #define HABIT_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Habit
+{
+private:
+	string name;
+	int streak;
+	bool completed;
+
+public:
+	Habit(string& n, int&s){}
+	bool maskDone();
+	bool resetDaily();
+	void display() const;
+	string serialize() const;
+	static Habit deserialize(const string& line);
+	bool isCompleted()const;
+	string getName()const;
+	int getStreak()const;
+};
+
 #endif
